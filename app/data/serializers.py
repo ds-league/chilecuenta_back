47d8bde1c7ad56348abb1c8a1da33acff4c760a2
Cristobal <cridonoso@udec.cl>
2020-01-22 10:27:42 -0500

    flake8 error solved

--- app/data/serializers.py
+++ app/data/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-
-from core.models import Family, Expense, People # importing model
+from core.models import Family, Expense, People  # importing model
 
 
 class FamilySerializer(serializers.ModelSerializer):
@@ -9,6 +8,7 @@ class FamilySerializer(serializers.ModelSerializer):
         fields = ['index', 'index_exp', 'folio',  'ingreso',
                   'fe', 'np', 'gasto',  'ingreso_disp', 'ptge_gasto']
 
+
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
@@ -16,8 +16,9 @@ class ExpenseSerializer(serializers.ModelSerializer):
                  'c5', 'c6', 'c7', 'c8', 'c9',
                  'c10', 'c11', 'c12', 'ahorro_deuda', 'ingreso']
 
+
 class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
         model = People
-        field = ['folio','inga_hd','fe','persona',
-                 'npersonas','edad','ingreso_hd','ingreso_pc']
+        field = ['folio', 'inga_hd', 'fe', 'persona',
+                 'npersonas', 'edad', 'ingreso_hd', 'ingreso_pc']
--- app/data/views.py
+++ app/data/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from core.models import Family, Expense, People
-from data.serializers import FamilySerializer, ExpenseSerializer, PeopleSerializer
+from data.serializers import FamilySerializer, ExpenseSerializer,\
+                             PeopleSerializer
 
 
 class FamilyViewSet(viewsets.ModelViewSet):
-    queryset = Family.objects.all() # iterator
+    queryset = Family.objects.all()  # iterator
     serializer_class = FamilySerializer
 
     # custom rute made by me
@@ -17,10 +17,12 @@ class FamilyViewSet(viewsets.ModelViewSet):
         edad_new = int(edad)*10000
         return Response(edad_new)
 
+
 class ExpenseViewSet(viewsets.ModelViewSet):
-    queryset = Expense.objects.all() # iterator
+    queryset = Expense.objects.all()  # iterator
     serializer_class = ExpenseSerializer
 
+
 class PeopleViewSet(viewsets.ModelViewSet):
-    queryset = People.objects.all() # iterator
+    queryset = People.objects.all()  # iterator
     serializer_class = PeopleSerializer
